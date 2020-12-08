88903c061d2128a26acad92daa2e50b389023708
Nayeli Gutiérrez <n.isillaure@gmail.com>
2020-12-07 21:47:45 -0600

    Added basic structure

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
 console.log('ola ke ase')
-var nombreVariable;
-let nombreVariable2
-let nombreVariable = 'Algo'
-let número = 1000
-let booleano = true
-let myArray = [1,2]
-let myOject = {
-    nombre: 'Nayeli',
-}
