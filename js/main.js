0653eade703cd700568124432f31869c3963ff49
Nayeli Gutiérrez <n.isillaure@gmail.com>
2020-12-07 21:34:34 -0600

    Added class-1 notes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log('ola ke ase')
+var nombreVariable;
+let nombreVariable2
+let nombreVariable = 'Algo'
+let número = 1000
+let booleano = true
+let myArray = [1,2]
+let myOject = {
+    nombre: 'Nayeli',
+}
