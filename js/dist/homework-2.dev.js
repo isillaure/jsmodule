f0b01dd946e459163f0eb18dcfd6f0be9215bdfc
Nayeli Gutiérrez <n.isillaure@gmail.com>
2020-12-10 19:10:08 -0600

    Added homework-2.js

---
+++ js/dist/homework-2.dev.js
@@ -0,0 +1,28 @@
+// Exercise 1
+// 1. pedir al usuario la calificación de un examen ( 0 a 100) 
+// Imprimir en consola las equivalencias en letra su calificación, es decir
+// -> si es mayor o igual a 90, imprimir "A"
+// -> si es mayor o igual a 80 y menor que 90 , imprimir "B"
+// -> si es mayor o igual a 70 y menor que 80 , imprimir "C"
+// -> si es mayor o igual a 60 y menor que 70 , imprimir "D"
+// -> si es mayor o igual a 50 y menor que 60 , imprimir "E"
+// -> si es menor 50 , imprimir "F"
+// Exercise 2
+// Pedir al usuario por prompt un numero entre 1 y 100
+// Comparar si ese numero es divisible entre 2 y 11 e imprimir en consola un mensaje indicandolo
+// sino, enviar un mensaje negandolo
+// Exercise 3
+// Pedir al usuario por prompt un numero entre 1 y 100
+// Verificar si es un par o impar
+// Imprimir en resultado en consola
+// Exercise 4
+// Pedir al usuario un numero entre 1 y 7 
+// Imprimir en consola el dia de la semana equivalente
+// es decir:
+// 1 -> lunes, 2 -> martes, etc.
+// Exercise 5
+// Pedir al usuario un numero entre 1 y 12
+// Imprimir en consola el numero de días que corresponden a ese mes
+// p.ej.
+// 5 -> 31 días
+"use strict";
