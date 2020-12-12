fa45fb26c30ca1f0b8fd2e8647eae65c2618eeb0
Nayeli Gutiérrez <n.isillaure@gmail.com>
2020-12-11 18:41:21 -0600

    Added 1 to 4 exercises in homework-2.js

--- js/dist/homework-2.dev.js
+++ js/dist/homework-2.dev.js
@@ -9,27 +9,29 @@
 // -> si es mayor o igual a 60 y menor que 70 , imprimir "D"
 // -> si es mayor o igual a 50 y menor que 60 , imprimir "E"
 // -> si es menor 50 , imprimir "F"
-// const GRADE = parseInt(prompt('Ingrese calificación de examen (0 a 100)'))
-// if (GRADE >= 90) {
-//     console.log('La equivalencia de la calificación es A')
-// } else if (90 > GRADE && GRADE >= 80) {
-//     console.log('La equivalencia de la calificación es B')
-// } else if (80 > GRADE && GRADE >=70) {
-//     console.log('La equivalencia de la calificación es C')
-// } else if (70 > GRADE && GRADE >=60) {
-//     console.log('La equivalencia de la calificación es D')
-// } else if (60 > GRADE && GRADE >=50) {
-//     console.log('La equivalencia de la calificación es E')
-// } else {
-//     console.log('La equivalencia de la calificación es F')
-// }
-// Exercise 2
+var GRADE = parseInt(prompt('Ingrese calificación de examen (0 a 100)'));
+
+if (GRADE >= 90) {
+  console.log('La equivalencia de la calificación es A');
+} else if (90 > GRADE && GRADE >= 80) {
+  console.log('La equivalencia de la calificación es B');
+} else if (80 > GRADE && GRADE >= 70) {
+  console.log('La equivalencia de la calificación es C');
+} else if (70 > GRADE && GRADE >= 60) {
+  console.log('La equivalencia de la calificación es D');
+} else if (60 > GRADE && GRADE >= 50) {
+  console.log('La equivalencia de la calificación es E');
+} else {
+  console.log('La equivalencia de la calificación es F');
+} // Exercise 2
 // Pedir al usuario por prompt un numero entre 1 y 100
 // Comparar si ese numero es divisible entre 2 y 11 e imprimir en consola un mensaje indicandolo
 // sino, enviar un mensaje negandolo
-var DIVISIBLE = parseInt(prompt('Ingrese un número del 0 a 100.'));
 
-if (DIVISIBLE % 2 && DIVISIBLE % 11 === 0) {
+
+var DIVISIBLE = parseInt(prompt('Ingrese un número del 0 a 100 para ver si es divisible entre 2 y 11.'));
+
+if (DIVISIBLE % 2 === 0 && DIVISIBLE % 11 === 0) {
   console.log('El número es divisible entre 2 y 11');
 } else {
   console.log('El número no es divisible entre 2 y 11');
@@ -37,12 +39,55 @@ if (DIVISIBLE % 2 && DIVISIBLE % 11 === 0) {
 // Pedir al usuario por prompt un numero entre 1 y 100
 // Verificar si es un par o impar
 // Imprimir en resultado en consola
-// Exercise 4
+
+
+var EVEN = parseInt(prompt('Ingrese un número del 0 a 100 para ver si es par.'));
+
+if (EVEN % 2 === 0) {
+  console.log('El número es par');
+} else {
+  console.log('El número no impar');
+} // Exercise 4
 // Pedir al usuario un numero entre 1 y 7 
 // Imprimir en consola el dia de la semana equivalente
 // es decir:
 // 1 -> lunes, 2 -> martes, etc.
-// Exercise 5
+
+
+var DAYWEEK = parseInt(prompt('Ingrese un número del 1 al 7'));
+
+switch (DAYWEEK) {
+  case '1':
+    console.log('El día es lunes');
+    break;
+
+  case '2':
+    console.log('El día es martes');
+    break;
+
+  case '3':
+    console.log('El día es miércoles');
+    break;
+
+  case '4':
+    console.log('El día es jueves');
+    break;
+
+  case '5':
+    console.log('El día es viernes');
+    break;
+
+  case '6':
+    console.log('El día es sábado');
+    break;
+
+  case '7':
+    console.log('El día es domingo');
+    break;
+
+  default:
+    var DAYWEEKTWO = parseInt(prompt('Ingrese un número del 1 al 7'));
+} // Exercise 5
 // Pedir al usuario un numero entre 1 y 12
 // Imprimir en consola el numero de días que corresponden a ese mes
 // p.ej.
--- js/homework-2.js
+++ js/homework-2.js
@@ -8,40 +8,44 @@
 // -> si es mayor o igual a 50 y menor que 60 , imprimir "E"
 // -> si es menor 50 , imprimir "F"
 
-// const GRADE = parseInt(prompt('Ingrese calificación de examen (0 a 100)'))
-// if (GRADE >= 90) {
-//     console.log('La equivalencia de la calificación es A')
-// } else if (90 > GRADE && GRADE >= 80) {
-//     console.log('La equivalencia de la calificación es B')
-// } else if (80 > GRADE && GRADE >=70) {
-//     console.log('La equivalencia de la calificación es C')
-// } else if (70 > GRADE && GRADE >=60) {
-//     console.log('La equivalencia de la calificación es D')
-// } else if (60 > GRADE && GRADE >=50) {
-//     console.log('La equivalencia de la calificación es E')
-// } else {
-//     console.log('La equivalencia de la calificación es F')
-// }
+const GRADE = parseInt(prompt('Ingrese calificación de examen (0 a 100)'))
+if (GRADE >= 90) {
+    console.log('La equivalencia de la calificación es A')
+} else if (90 > GRADE && GRADE >= 80) {
+    console.log('La equivalencia de la calificación es B')
+} else if (80 > GRADE && GRADE >=70) {
+    console.log('La equivalencia de la calificación es C')
+} else if (70 > GRADE && GRADE >=60) {
+    console.log('La equivalencia de la calificación es D')
+} else if (60 > GRADE && GRADE >=50) {
+    console.log('La equivalencia de la calificación es E')
+} else {
+    console.log('La equivalencia de la calificación es F')
+}
 
 
 // Exercise 2
 // Pedir al usuario por prompt un numero entre 1 y 100
 // Comparar si ese numero es divisible entre 2 y 11 e imprimir en consola un mensaje indicandolo
 // sino, enviar un mensaje negandolo
-const DIVISIBLE = parseInt(prompt('Ingrese un número del 0 a 100.'))
-if (DIVISIBLE % 2 && DIVISIBLE % 11 === 0) {
+const DIVISIBLE = parseInt(prompt('Ingrese un número del 0 a 100 para ver si es divisible entre 2 y 11.'))
+if (DIVISIBLE % 2 === 0 && DIVISIBLE % 11 === 0){
     console.log('El número es divisible entre 2 y 11')
 } else {
     console.log('El número no es divisible entre 2 y 11')
 }
 
 
-
 // Exercise 3
 // Pedir al usuario por prompt un numero entre 1 y 100
 // Verificar si es un par o impar
 // Imprimir en resultado en consola
-
+const EVEN = parseInt(prompt('Ingrese un número del 0 a 100 para ver si es par.'))
+if (EVEN % 2 === 0){
+    console.log('El número es par')
+} else {
+    console.log('El número no impar')
+}
 
 
 // Exercise 4
@@ -49,7 +53,32 @@ if (DIVISIBLE % 2 && DIVISIBLE % 11 === 0) {
 // Imprimir en consola el dia de la semana equivalente
 // es decir:
 // 1 -> lunes, 2 -> martes, etc.
-
+const DAYWEEK = parseInt(prompt('Ingrese un número del 1 al 7'))
+switch (DAYWEEK) {
+    case '1':
+        console.log('El día es lunes')
+        break;
+    case '2':
+        console.log('El día es martes')
+        break;
+    case '3':
+        console.log('El día es miércoles')
+        break;
+    case '4':
+        console.log('El día es jueves')
+        break;
+    case '5':
+        console.log('El día es viernes')
+        break;       
+    case '6':
+        console.log('El día es sábado')
+        break;
+    case '7':
+        console.log('El día es domingo')
+        break;
+    default:
+        const DAYWEEKTWO = parseInt(prompt('Ingrese un número del 1 al 7'))
+}
 
 
 // Exercise 5
