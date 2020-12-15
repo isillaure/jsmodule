d39d863ebdc5766349d3b17c3e25124a33069df5
Nayeli Gutiérrez <n.isillaure@gmail.com>
2020-12-14 20:39:50 -0600

    Added arrow_functions.js

--- js/dist/main.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function add(a, b) {
-  var c = a + b;
-  return c; // Return termina la ejecución del programa, con return termina la función, después de return ya no se ejecuta el código que sigue.
-  // Devuelve el valor en vez de imprimirlo con console.log.
-}
-
-var result = add(2, 5); // result -> 7
