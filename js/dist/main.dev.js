357dde627292f8b6590bbd252d1bacffbd750c70
Nayeli Gutiérrez <n.isillaure@gmail.com>
2020-12-14 20:04:11 -0600

    Added basic structure

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -1,3 +1,9 @@
 "use strict";
 
-console.log('ola ke ase');
+function add(a, b) {
+  var c = a + b;
+  return c; // Return termina la ejecución del programa, con return termina la función, después de return ya no se ejecuta el código que sigue.
+  // Devuelve el valor en vez de imprimirlo con console.log.
+}
+
+var result = add(2, 5); // result -> 7
--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log('ola ke ase')
+function add(a,b) {
+
+    const c = a + b
+    return c // Return termina la ejecución del programa, con return termina la función, después de return ya no se ejecuta el código que sigue.
+    // Devuelve el valor en vez de imprimirlo con console.log.
+}
+
+const result = add(2,5)
+// result -> 7
